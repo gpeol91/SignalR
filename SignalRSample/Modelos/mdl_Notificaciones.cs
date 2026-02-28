082ef86be824d34b2492299d47afb5d470f3488f
SistemasSinaloa <sistemassinaloa@humaya.com.mx>
2026-02-27 17:56:33 -0700

    act

--- SignalRSample/Modelos/mdl_Notificaciones.cs
+++ SignalRSample/Modelos/mdl_Notificaciones.cs
@@ -2,6 +2,7 @@
 {
     public class mdl_Notificaciones
     {
+        public int idMensaje { get; set; }
         public string? numeroTelefono { get; set; }
         public string? cliente { get; set; }
         public string? mensaje { get; set; }
